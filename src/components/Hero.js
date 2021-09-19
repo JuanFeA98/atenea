3497bdc6c7f7da1e61752aa8976f5b078c7e140c
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-09-19 12:30:37 -0500

    Slider I

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,52 @@ import React from 'react'
 import '../styles/hero.css'
 
 export default function Hero() {
+    const [checked, setChecked] = React.useState(true)
+    
+    const toogleCheck = ()=>{
+        setChecked(false)
+    }
+
+    const toogleCheckTrue = ()=>{
+        setChecked(true)
+    }
+
     return (
-        <section className='Hero'>
-            <div className="Hero-Carousel">
-                Hello
+        <React.Fragment>
+            {/* <section className='Hero'>
+                <div className="Hero-Carousel">
+                    <div className="Hero-Carousel-1"></div>
+                </div>
+            </section> */}
+            <div className="slider">
+                <input 
+                    type="radio" 
+                    name="slider" 
+                    onClick={toogleCheckTrue}
+                    checked={checked ? "checked": false}
+                />
+                <div className="imgBox">
+                    <img src="https://cdn.pixabay.com/photo/2020/04/25/12/14/tech-5090539_1280.jpg" alt="" />
+                </div>
+                <input 
+                    type="radio" 
+                    name="slider"
+                    onClick={toogleCheck}
+
+                />
+                <div className="imgBox">
+                    <img src="https://cdn.pixabay.com/photo/2021/08/07/08/49/building-6528075_1280.jpg" alt="" />
+                </div>
+                <input 
+                    type="radio" 
+                    name="slider"
+                    onClick={toogleCheck}
+
+                />
+                <div className="imgBox">
+                    <img src="https://cdn.pixabay.com/photo/2019/03/12/18/53/city-4051537_1280.jpg" alt="" />
+                </div>
             </div>
-        </section>
+        </React.Fragment>
     )
 }
